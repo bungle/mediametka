25df7b888422862413841632630ad22c8ae52f25
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2014-03-27 19:57:00 +0200

    Changed vertical center to js.

--- html/js/main.js
+++ html/js/main.js
@@ -1,1 +1,12 @@
+$(function() {
+    $(window).resize(function() {
+        verticalCenter();
+    });
+    verticalCenter();
+});
 
+function verticalCenter() {
+    var caption = $('.carousel-caption')
+    var bottom = ($('.carousel').height() - caption.height()) / 2;
+    caption.css({ 'bottom' : bottom });
+}
